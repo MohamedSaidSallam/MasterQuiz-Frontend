d76f0cd0c5e2fea553582d80899a08b89baad1af
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-08-18 16:29:25 +0200

    feat: show generes from typescript

--- src/app/genres/genres.component.ts
+++ src/app/genres/genres.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenresComponent implements OnInit {
 
+  genres = ['Genre 1', 'Genre 2', 'Genre 3', 'Genre 4', 'Genre 5', 'Genre 6', 'Genre 7', 'Genre 8'];
+
   constructor() { }
 
   ngOnInit(): void {
