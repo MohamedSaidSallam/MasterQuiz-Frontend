3075eee5af128e746106f5982f133bce8e72e5e2
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-08-18 16:29:42 +0200

    feat: show cards from typescript

--- src/app/popular/popular.component.ts
+++ src/app/popular/popular.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-popular',
   templateUrl: './popular.component.html',
-  styleUrls: ['./popular.component.scss']
+  styleUrls: ['./popular.component.scss'],
 })
 export class PopularComponent implements OnInit {
+  quizzes = [{
+    genres: ['Tag 1', 'Tag 2', 'Tag 3'],
+    name: 'Quiz Title',
+    description: 'Quiz Description with filler text',
+  },
+  {
+    genres: ['Tag 1', 'Tag 2', 'Tag 3'],
+    name: 'Quiz Title',
+    description: 'Quiz Description with filler text',
+  },
+  {
+    genres: ['Tag 1', 'Tag 2', 'Tag 3'],
+    name: 'Quiz Title',
+    description: 'Quiz Description with filler text',
+  },
+  {
+    genres: ['Tag 1', 'Tag 2', 'Tag 3'],
+    name: 'Quiz Title',
+    description: 'Quiz Description with filler text',
+  }];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
