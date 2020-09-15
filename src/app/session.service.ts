f3ac3f2590c18439feec28cda5b554c1201c80c0
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-09-15 19:20:43 +0200

    fix: remove debug variable

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -2,37 +2,37 @@ import { Injectable } from '@angular/core';
 import { Socket, SocketIoConfig } from 'ngx-socket-io';
 import { Observable } from 'rxjs';
 
-export class SocketNameSpace extends Socket{
-  constructor(socketConfig: SocketIoConfig){
+export class SocketNameSpace extends Socket {
+  constructor(socketConfig: SocketIoConfig) {
     super(socketConfig);
   }
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class SessionService {
   room: SocketNameSpace;
 
-  constructor(private code : string) {
-    const code = 'fsf' 
-    this.room  = new SocketNameSpace({url: `http://localhost:3000/session/${code}` });
+  constructor(private code: string) {
+    this.room = new SocketNameSpace({
+      url: `http://localhost:3000/session/${code}`,
+    });
   }
 
-  foo (){
-    this.room.emit('answer', 'hi ana msg')
+  foo(): void {
+    this.room.emit('answer', 'hi ana msg');
   }
 
-  public sendMessage(message) {
+  public sendMessage(message): void {
     this.room.emit('msg', message);
-}
+  }
 
   public getMessages = () => {
-      return Observable.create((observer) => {
-          this.room.on('msg', (message) => {
-              observer.next(message);
-          });
+    return Observable.create((observer) => {
+      this.room.on('msg', (message) => {
+        observer.next(message);
       });
+    });
   }
 }
